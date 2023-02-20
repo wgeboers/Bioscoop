13d895a9f8ec707796732f37283445beb5951f2d
wgeboers <wesleygeboers@outlook.com>
2023-02-20 21:55:07 +0100

    Bioscoop.Test
    + Project for unit testing

---
+++ Bioscoop.Test/MoviesControllerTest.cs
@@ -0,0 +1,16 @@
+using Bioscoop.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bioscoop.Test
+{
+    public class MoviesControllerTest
+    {
+        MovieController _controller;
+
+        [Fact]
+        public void GetAllTest()
+        {
+            
+        }
+    }
+}
