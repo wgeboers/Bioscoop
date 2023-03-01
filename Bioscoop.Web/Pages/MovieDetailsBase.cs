7ee886321373ee34df120b70be585b9b858ca7d7
Thijs <twgr.lammerts@student.avans.nl>
2023-03-01 01:06:26 +0100

    Added MovieDetails page

---
+++ Bioscoop.Web/Pages/MovieDetailsBase.cs
@@ -0,0 +1,31 @@
+﻿using Bioscoop.Models.Dtos;
+using Bioscoop.Web.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+
+namespace Bioscoop.Web.Pages
+{
+    public class MovieDetailsBase:ComponentBase
+    {
+        [Parameter]
+        public int Id { get; set; }
+        [Inject]
+        public IMovieService MovieService { get; set; }
+
+        public MovieDto Movie { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                Movie = await MovieService.GetMovie(Id);
+            }
+            catch (Exception ex)
+            {
+
+                ErrorMessage = ex.Message;
+            }
+        }
+    }
+}
--- Bioscoop.Web/Program.cs
+++ Bioscoop.Web/Program.cs
@@ -1,4 +1,6 @@
 using Bioscoop.Web;
+using Bioscoop.Web.Services;
+using Bioscoop.Web.Services.Contracts;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7027/") });
+builder.Services.AddScoped<IMovieService, MovieService>();
 
 await builder.Build().RunAsync();
