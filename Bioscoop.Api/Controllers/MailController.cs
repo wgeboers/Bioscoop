41ff86291cb80ed48433788cfcd84c7c66e7b543
MarcelForman <91687704+MarcelForman@users.noreply.github.com>
2023-03-02 20:48:04 +0100

    +added mailAPI
    +MailKit Api for sending mail, no attachment

---
+++ Bioscoop.Api/Controllers/MailController.cs
@@ -0,0 +1,33 @@
+﻿using Bioscoop.Api.Models;
+using Bioscoop.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bioscoop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        private readonly Services.IMailService _mail;
+
+        public MailController(Services.IMailService mail)
+        {
+            _mail = mail;
+        }
+
+        [HttpPost("sendmail")]
+        public async Task<IActionResult> SendMailAsync(MailData mailData)
+        {
+            bool result = await _mail.SendAsync(mailData, new CancellationToken());
+
+            if (result)
+            {
+                return StatusCode(StatusCodes.Status200OK, "Mail has successfully been sent.");
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occured. The Mail could not be sent.");
+            }
+        }
+    }
+}
---
+++ Bioscoop.Api/Services/IMailService.cs
@@ -0,0 +1,9 @@
+﻿using Bioscoop.Api.Models;
+
+namespace Bioscoop.Api.Services
+{
+    public interface IMailService
+    {
+        Task<bool> SendAsync(MailData mailData, CancellationToken ct);
+    }
+}
