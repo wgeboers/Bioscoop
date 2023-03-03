20d57ea2b6857486eed42ec94b528b19f3293485
ThomasD92 <thomas.daane@hotmail.com>
2023-03-03 23:33:39 +0100

    Big errors eruit
    Alleen showRowList error.
    Met heleboel non nullable references.

---
+++ Bioscoop.Web/pages/DisplayshowListBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using Bioscoop.Models.Dtos;
+
+
+namespace Bioscoop.Web.pages
+{
+    public class DisplayshowListBase : ComponentBase
+    {
+        [Parameter]
+        public IEnumerable<ShowDto> Shows { get; set; }
+    }
+}
