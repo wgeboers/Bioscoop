610e5087533fd65b71ee1cde3ed9f5dafec2a510
Cslow <34058689+Cslow@users.noreply.github.com>
2023-03-05 09:25:02 +0100

    Add files via upload

---
+++ Bioscoop.Web/wwwroot/Script.js
@@ -0,0 +1,10 @@
+﻿
+
+function initialize() {
+    var latlng = new google.maps.LatLng(51.583957, 4.795526);
+    var options = {
+        zoom: 14, center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), options);
+}
