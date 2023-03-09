9c6c6c02c66ebe18479e0efa2cf46df91bf8c8b7
TDAA <trbl.daane@student.avans.nl>
2023-03-09 19:19:07 +0100

    Build success met Iticketservice <TicketDto> toevoeging

--- Bioscoop.Web/Services/Contracts/ITicketService.cs
+++ Bioscoop.Web/Services/Contracts/ITicketService.cs
@@ -4,6 +4,7 @@ namespace Bioscoop.Web.Services.Contracts
 {
     public interface ITicketService
     {
+        Task<TicketDto> AddTicket(TicketToAddDto ticketToAddDto);
         Task<TicketDto> GetTicket(int id);
     }
 }
