c559b5aa83c6e230e921e1bb24327403241ab614
TDAA <trbl.daane@student.avans.nl>
2023-03-10 09:43:51 +0100

    Program web URI aangepast

--- Bioscoop.Web/Program.cs
+++ Bioscoop.Web/Program.cs
@@ -9,7 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5158/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7027/") });
 builder.Services.AddScoped<IMovieService, MovieService>();
 builder.Services.AddScoped<IShowService, ShowService>();
 builder.Services.AddScoped<ITicketService, TicketService>();
