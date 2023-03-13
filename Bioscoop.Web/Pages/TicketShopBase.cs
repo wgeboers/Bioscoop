1a5359d0187c3bc74f1251c752db5c0303ce18bc
wgeboers <wesleygeboers@outlook.com>
2023-03-13 23:05:43 +0100

    Seat selection & deselection
    + visual for selected chair
    + creation off chair objects
    + retrieve chair number & row number

--- Bioscoop.Web/Pages/TicketShopBase.cs
+++ Bioscoop.Web/Pages/TicketShopBase.cs
@@ -1,6 +1,7 @@
 ﻿using Bioscoop.Models.Dtos;
 using Bioscoop.Web.Services.Contracts;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace Bioscoop.Web.Pages
 {
@@ -18,11 +19,16 @@ namespace Bioscoop.Web.Pages
         [Inject]
         public ITicketService TicketService { get; set; }
 
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
 
         public ShowDto Show { get; set; }
         public IEnumerable<TicketDto> Tickets { get; set; }
         public string ErrorMessage { get; set; }
 
+        public List<Seat> Seats { get; set; }
+
+        public List<Seat> SelectedSeats { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
@@ -30,6 +36,8 @@ namespace Bioscoop.Web.Pages
             {
                 Show = await ShowService.GetShow(Id);
                 Tickets = await TicketService.GetTicketsByShow(Id);
+                Seats = new List<Seat>();
+                SelectedSeats = new List<Seat>();
             }
             catch (Exception ex)
             {
@@ -50,6 +58,20 @@ namespace Bioscoop.Web.Pages
             {
 
                 //Log Exception
+            }
+        }
+
+        public class Seat
+        {
+            public int seatnr { get; set; }
+            public int rownr { get; set; }
+            public string type { get; set; }
+
+            public Seat(int seatnrCon, int rownrCon, string type)
+            {
+                this.seatnr = seatnrCon;
+                this.rownr = rownrCon;
+                this.type = type;
             }
         }
     }
